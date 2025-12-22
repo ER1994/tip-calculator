f679b83f2a312d1cfeab7826ee529c312388bd93
ER1994 <ewkarado@gmail.com>
2025-12-22 23:21:16 +0100

    Implement full UI redesign: add styles in `main.css` and `variables.css`, update validation and default value handling in `calculations.js`, and enhance structure and semantics in `index.html`.

--- calculations.js
+++ calculations.js
@@ -2,11 +2,14 @@ const tipBtns = document.querySelectorAll('.tip-btn')
 const tipInput = document.querySelectorAll('.tip-input')
 const billAmount = document.getElementById('bill')
 const people = document.getElementById('people')
+const resetBtn = document.getElementById('reset')
+const errorMessage = document.querySelector('.error-message__inactive')
 
-const validations = {
-    bill: (value) => value !== '0',
-    people: (value) => value !== '0'
-}
+const
+    validations = {
+        bill: (value) => value !== '0',
+        people: (value) => value !== '0'
+    }
 
 function dataIsValid(name, value, validations) {
     if (!validations[name]) return true
@@ -19,6 +22,11 @@ function isValidForm(form, validations) {
     inputs.forEach(input => {
         if (!dataIsValid(input.name, input.value, validations)) {
             isValid = false
+            input.classList.toggle('input--invalid')
+            errorMessage.classList.toggle('error-message__active')
+            errorMessage.classList.toggle('error-message__inactive')
+        } else {
+            input.classList.toggle('input--valid')
         }
     })
     return isValid
@@ -26,12 +34,12 @@ function isValidForm(form, validations) {
 
 function updateTipPerson(amount) {
     const totalTip = document.getElementById('result-tip')
-    totalTip.value = parseFloat(amount.toFixed(2))
+    totalTip.value = '$' + amount.toFixed(2)
 }
 
 function updateTotalPerson(amount) {
     const total = document.getElementById('result-total')
-    total.value = parseFloat(amount.toFixed(2))
+    total.value = '$' + amount.toFixed(2)
 }
 
 function calculateTotals(form, rate) {
@@ -49,6 +57,7 @@ function tipFormHandler(target, form) {
     const formEvent = document.getElementById('calculations-forms')
     let rate = form === 'button' ? parseFloat(target.dataset.rate) : parseFloat(target.value) / 100
     calculateTotals(formEvent, rate)
+    resetBtn.classList.toggle('btn--selected')
 }
 
 people.addEventListener('keypress', (e) => {
@@ -57,16 +66,41 @@ people.addEventListener('keypress', (e) => {
     }
 })
 
+people.addEventListener('change', (e) => {
+    if (!dataIsValid(people.name, people.value, validations)) {
+        people.classList.toggle('input--invalid')
+    } else {
+        people.classList.toggle('input--valid')
+    }
+})
+
 billAmount.addEventListener('change', (e) => {
     let value = parseFloat(e.target.value)
     e.target.value = +value > 0 ? parseFloat(value.toFixed(2)) : 0
+    if (!dataIsValid(billAmount.name, billAmount.value, validations)) {
+        billAmount.classList.toggle('input--invalid')
+    } else {
+        billAmount.classList.toggle('input--valid')
+    }
 })
 
 tipBtns.forEach(btn => {
     btn.addEventListener('click', (e) => tipFormHandler(e.target, 'button'))
-
+    btn.addEventListener('click', (e) => {
+        btn.classList.toggle('btn--selected')
+    })
 })
 
 tipInput.forEach(input => {
     input.addEventListener('change', (e) => tipFormHandler(e.target, 'input'))
+})
+
+resetBtn.addEventListener('click', (e) => {
+    tipBtns.forEach(btn => {
+        btn.classList.remove('btn--selected')
+    })
+    errorMessage.classList.remove('error-message__active')
+    billAmount.classList.remove('input--invalid')
+    people.classList.remove('input--invalid')
+    errorMessage.classList.add('error-message__inactive')
 })
